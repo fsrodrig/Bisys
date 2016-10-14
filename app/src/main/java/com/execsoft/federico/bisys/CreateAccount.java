606b1a003764cc25d085146ad8019a79fe7f58cd
federico <fs.rodriguez91@gmail.com>
2016-10-14 19:06:20 -0300

    Diseño Crear Cuenta

--- app/src/main/java/com/execsoft/federico/bisys/CreateAccount.java
+++ app/src/main/java/com/execsoft/federico/bisys/CreateAccount.java
@@ -3,11 +3,11 @@ package com.execsoft.federico.bisys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account);
     }
 }
